8c030430132b0f4d476626f91fcafb997a33f4ec
yrs147 <yashraj14700728@gmail.com>
2023-03-29 18:01:21 +0530

    initial commit

---
+++ target/debug/deps/compress_youtube-888ff38e0e0521f7.d
@@ -0,0 +1,5 @@
+c:\Users\Yash\rust\compress_youtube\target\debug\deps\compress_youtube-888ff38e0e0521f7.rmeta: src\main.rs
+
+c:\Users\Yash\rust\compress_youtube\target\debug\deps\compress_youtube-888ff38e0e0521f7.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/compress_youtube-e1e061a5ee1c73db.d
@@ -0,0 +1,5 @@
+c:\Users\Yash\rust\compress_youtube\target\debug\deps\compress_youtube-e1e061a5ee1c73db.rmeta: src\main.rs
+
+c:\Users\Yash\rust\compress_youtube\target\debug\deps\compress_youtube-e1e061a5ee1c73db.d: src\main.rs
+
+src\main.rs:
